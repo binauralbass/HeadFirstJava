0323b2845681b495e513bdfbdd89c1133129b54c
binauralbass <binauralbass@gmail.com>
2021-11-08 11:03:22 +0900

    p.48

--- BeerSong/BeerSong.java
+++ BeerSong/BeerSong.java
@@ -6,8 +6,8 @@ public class BeerSong{
     while(beerNum>0){
       if(beerNum==1){
         word="bottle";
-      } 
-
+      }
+      
       System.out.println(beerNum+" "+word+" of beer on the wall");
       System.out.println(beerNum+" "+word+" of beer.");
       System.out.println("Take one down");
